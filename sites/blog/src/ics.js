c20899829c7c99c509dafbfbf9cfa3663bb00685
signalwerk <sh@signalwerk.ch>
2020-05-14 01:53:21 +0200

    ADD: ics

--- sites/blog/src/ics.js
+++ sites/blog/src/ics.js
@@ -5,5 +5,6 @@ let ics = new Semesterplan();
 ics.addTeachers("./src/data/IAD2019/teachers.yaml");
 ics.add("./src/data/IAD2019/semester2019HS.yaml");
 ics.add("./src/data/IAD2019/semester2020FS.yaml");
+ics.add("./src/data/IAD2019/semester2020HS.yaml");
 ics.ics("./public/IAD.ics");
 console.log("-- end ics generation --");
